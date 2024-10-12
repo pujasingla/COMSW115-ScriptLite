7f3822b7c64316674b70a8d10f3657ac8319b323
Ria <xl3466@columbia.edu>
2024-10-12 15:11:54 -0400

    Initialize scanner

---
+++ scanner.py
@@ -0,0 +1,35 @@
+SEPARATORS = {
+    '(': 'LPAR',
+    ')': 'RPAR',
+    '{': 'LBRKT',
+    '}': 'RBRKT',
+    '[': 'LSQBRKT',
+    ']': 'RSQBRKT',
+    ',': 'COMMA',
+    ';': 'SEMICOLON'
+}
+
+OPERATORS = {
+    '+': 'PLUS',
+    '=': 'EQUAL'
+}
+
+class Scanner:
+    def __init__(self):
+        self.cursor = 0
+    
+    def advance(self):
+        self.cursor += 1
+    
+    def reset_cursor(self):
+        self.cursor = 0
+    
+    def scan(self, program):
+        for line in program.split("\n"):
+            self.reset_cursor()
+            c = line[self.cursor]
+            # look up state transitions
+
+
+if __name__== "__main__":
+    scanner = Scanner()
